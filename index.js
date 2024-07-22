b458c7ca6360b26b429dd00b8eb30b708a5a094e
luis <110468848+LowisWano@users.noreply.github.com>
2024-07-22 22:14:14 +0800

    feat: finished exercises 3.1-3.6

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
       "id": "1",
@@ -24,11 +26,6 @@ let persons = [
     }
 ]
 
-
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
-})
-
 app.get('/info', (request, response)=>{
     const date = new Date();
     response.send(`
@@ -36,6 +33,54 @@ app.get('/info', (request, response)=>{
         <p>${date}</p>
     `);
 })
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id;
+    const person = persons.find(person => person.id === id);
+
+    person ? response.json(person): response.status(404).end();    
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+const generateID = () => {
+    return String(Math.floor(Math.random() * 10000000)) 
+}
+
+  app.post('/api/persons', (request, response) => {
+    const body = request.body;
+
+    if(!body){
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    if(persons.find((person) => person.name === body.name)){
+        return response.status(409).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const person = {
+        id: generateID(),
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person);
+    console.log(person);
+    response.json(person);
+  })
 
 const PORT = 3001
 app.listen(PORT, () => {
